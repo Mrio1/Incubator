e107087979fdb8f2fe762e457905d4e9607289be
A. Zhur <sanchesmrio1@gmail.com>
2021-10-12 14:17:06 +0300

    init: start swapi project

---
+++ swapi/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'foo.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 600,
+  },
+};
