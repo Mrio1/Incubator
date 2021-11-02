a1f69c66f148b909a58f2c2f52b03a315c4a0b9f
A. Zhur <sanchesmrio1@gmail.com>
2021-11-02 17:30:24 +0300

    feat: add search and localstorage save data

---
+++ swapi/src/scripts/dictionary.js
@@ -0,0 +1,13 @@
+const navView = {
+    'formClassName': 'nav__form',
+    'formInput': 'form__input',
+    'navIdName': 'navigationPanel',
+    'navClassName': 'app__nav',
+    'categoryClassName': 'nav__item',
+    'categoryActiveClassName': 'nav__item--active',
+    'burgerActiveClassName': 'burger-active',
+    'burgerId': 'burgerButton'
+}
+
+
+export {navView};
--- swapi/src/scripts/navView.js
+++ swapi/src/scripts/navView.js
@@ -1,21 +1,46 @@
+import {navView} from './dictionary';
+
 class NavigationView {
   constructor(categoryClickCb) {
-    this.init();
-    this.categoryClickCb = categoryClickCb;
-  }
-
-  init() {
-    this.navBlock = document.getElementById("navigationPanel");
-    this.categoryList = document.querySelectorAll(".nav__item");
+    this.navBlock = document.getElementById(navView.navIdName);
+    this.categoryList = document.querySelectorAll(navView.categoryClassName);
     this.navBlock.addEventListener("click", this.navClickHandler.bind(this));
+    this.categoryClickCb = categoryClickCb;
+    this.burgerButton = document.getElementById(navView.burgerId);
+    this.burgerActiveWidth = 480;
+    this.burgerButton.addEventListener('click', this.burgerButtonClickHandler.bind(this));
+    this.navForm = document.querySelector(`.${navView.formClassName}`);
+    document.body.addEventListener('click', this.outNavClickHandler.bind(this))
+    window.addEventListener('resize', this.windowResizeHandler.bind(this));
+    this.navForm.addEventListener('submit', this.formSubmitHandler.bind(this));
   }
 
   navClickHandler({ target }) {
-    if (target.classList.contains("nav__item")) {
+    if (target.classList.contains(navView.categoryClassName)) {
       this.activateCategory(target);
       const category = target.dataset.category;
       this.categoryClickCb(category.toLowerCase());
     }
+  }
+
+  burgerButtonClickHandler() {
+    this.navBlock.classList.toggle(navView.burgerActiveClassName);
+  }
+
+  outNavClickHandler({target}) {
+    if (!target.closest(`.${navView.navClassName}`)) {
+      this.deactivateBurger();
+    }
+  }
+
+  windowResizeHandler() {
+    if(window.innerWidth > this.burgerActiveWidth) {
+      this.deactivateBurger();
+    }
+  }
+
+  deactivateBurger() {
+    this.navBlock.classList.remove(navView.burgerActiveClassName);
   }
 
   activateCategory(newActiveCategory) {
@@ -23,6 +48,14 @@ class NavigationView {
       category.classList.remove("nav__item--active");
     });
     newActiveCategory.classList.add("nav__item--active");
+    this.deactivateBurger();
+  }
+
+  formSubmitHandler(e) {
+    e.preventDefault();
+    const inputValue = e.target.querySelector(`.${navView.formInput}`).value;
+    this.categoryClickCb(null, inputValue);
+    this.navForm.reset();
   }
 }
 
